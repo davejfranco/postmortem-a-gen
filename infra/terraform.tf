c31fb98587897377edfcafe7b5cd84abbbe1c860
Dave Franco <self@daveops.sh>
2025-08-23 09:31:26 +0200

    depends on cluster being ready

--- infra/terraform.tf
+++ infra/terraform.tf
@@ -24,10 +24,12 @@ provider "aws" {
 
 data "aws_eks_cluster" "cluster" {
   name = module.eks.cluster_name
+  depends_on = [ module.eks ]
 }
 
 data "aws_eks_cluster_auth" "cluster" {
   name = module.eks.cluster_name
+  depends_on = [ module.eks ]
 }
 
 provider "kubernetes" {
