f92e4ad94d3ddd078a433933dca71a5d2bf0c858
Dave Franco <self@daveops.sh>
2025-08-23 09:09:56 +0200

    cluster fix

--- infra/main.tf
+++ infra/main.tf
@@ -19,9 +19,6 @@ module "vpc" {
   enable_dns_hostnames = true
   enable_dns_support   = true
 
-  create_database_subnet_group       = true
-  create_database_subnet_route_table = true
-
   tags = {
     Environment = var.environment
     Project     = var.project_name
@@ -34,10 +31,6 @@ module "vpc" {
 
   private_subnet_tags = {
     Type = "private"
-  }
-
-  database_subnet_tags = {
-    Type = "database"
   }
 }
 
@@ -60,74 +53,40 @@ data "aws_caller_identity" "current" {}
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "~> 21.0"
+  version = "21.1.0"
 
   name               = var.project_name
   kubernetes_version = var.cluster_version
 
-  vpc_id                   = module.vpc.vpc_id
-  control_plane_subnet_ids = module.vpc.public_subnets
-  subnet_ids = concat(
-    module.vpc.public_subnets,
-    module.vpc.private_subnets
-  )
-
-  endpoint_public_access       = true
-  endpoint_private_access      = true
-  endpoint_public_access_cidrs = var.allowed_cidr_blocks
-
+  # EKS Addons
   addons = {
-    coredns = {
-      most_recent = true
-    }
-    kube-proxy = {
-      most_recent = true
-    }
-    vpc-cni = {
-      most_recent = true
-    }
+    coredns = {}
     eks-pod-identity-agent = {
-      most_recent = true
+      before_compute = true
+    }
+    kube-proxy = {}
+    vpc-cni = {
+      # need it before trying to create node groups
+      before_compute = true
     }
   }
-  authentication_mode = "API"
-  # Enable cluster creator admin permissions
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  authentication_mode                      = "API"
+  endpoint_public_access                   = true
   enable_cluster_creator_admin_permissions = true
+  endpoint_public_access_cidrs             = var.allowed_cidr_blocks
 
-  # Access entries for additional users/roles
-  access_entries = {
-    cluster_creator = {
-      principal_arn = data.aws_caller_identity.current.arn
-      policy_associations = {
-        admin = {
-          policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
-          access_scope = {
-            type = "cluster"
-          }
-        }
-      }
+  eks_managed_node_groups = {
+    example = {
+      instance_types = var.node_group_instance_types
+
+      min_size     = var.node_group_min_size
+      max_size     = var.node_group_max_size
+      desired_size = var.node_group_desired_size
     }
   }
-
-  #eks_managed_node_groups = {
-  #  main = {
-  #    name = "nodes"
-
-  #    instance_types = var.node_group_instance_types
-
-  #    min_size     = var.node_group_min_size
-  #    max_size     = var.node_group_max_size
-  #    desired_size = var.node_group_desired_size
-
-  #    subnet_ids = module.vpc.public_subnets
-
-  #    tags = {
-  #      Environment = var.environment
-  #      Project     = var.project_name
-  #      Terraform   = "true"
-  #    }
-  #  }
-  #}
 
   tags = {
     Environment = var.environment
