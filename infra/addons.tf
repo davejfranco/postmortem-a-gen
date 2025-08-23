04b84d6e986e09c6eafcbc4c5ada45f064c1776e
Dave Franco <self@daveops.sh>
2025-08-23 09:31:42 +0200

    external-dns and external-secrets

--- infra/addons.tf
+++ infra/addons.tf
@@ -1,4 +1,48 @@
+module "aws_vpc_cni_ipv4_pod_identity" {
+  source  = "terraform-aws-modules/eks-pod-identity/aws"
+  version = "v2.0.0"
+
+  name = "aws-vpc-cni-ipv4"
+
+  attach_aws_vpc_cni_policy = true
+  aws_vpc_cni_enable_ipv4   = true
+
+  # Pod Identity Associations
+  association_defaults = {
+    namespace       = "kube-system"
+    service_account = "aws-node"
+  }
+
+  associations = {
+    postmortem-eks = {
+      cluster_name = module.eks.cluster_name
+    }
+  }
+  tags = {
+    Environment = "demo"
+  }
+}
+
 # external-secrets 
+module "external_secrets_pod_identity" {
+  source  = "terraform-aws-modules/eks-pod-identity/aws"
+  version = "v2.0.0"
+
+  name = "external-secrets"
+
+  attach_external_secrets_policy     = true
+  external_secrets_create_permission = true
+
+  associations = {
+    postmortem-eks = {
+      cluster_name = module.eks.cluster_name
+    }
+  }
+  tags = {
+    Environment = "demo"
+  }
+}
+
 data "aws_iam_policy_document" "external_secrets_assume_role" {
   statement {
     effect = "Allow"
@@ -18,7 +62,7 @@ data "aws_iam_policy_document" "external_secrets_assume_role" {
 data "aws_iam_policy_document" "external_secrets" {
   statement {
     effect = "Allow"
-    
+
     actions = [
       "secretsmanager:GetResourcePolicy",
       "secretsmanager:GetSecretValue",
@@ -26,20 +70,20 @@ data "aws_iam_policy_document" "external_secrets" {
       "secretsmanager:ListSecretVersionIds",
       "secretsmanager:ListSecrets"
     ]
-    
+
     resources = ["*"]
   }
 
   statement {
     effect = "Allow"
-    
+
     actions = [
       "ssm:GetParameter",
       "ssm:GetParameters",
       "ssm:GetParametersByPath",
       "ssm:DescribeParameters"
     ]
-    
+
     resources = ["*"]
   }
 }
@@ -85,10 +129,10 @@ resource "aws_eks_pod_identity_association" "external_secrets" {
 }
 
 resource "helm_release" "external_secrets" {
-  name       = "external-secrets"
-  repository = "https://charts.external-secrets.io"
-  chart      = "external-secrets"
-  namespace  = "external-secrets-system"
+  name             = "external-secrets"
+  repository       = "https://charts.external-secrets.io"
+  chart            = "external-secrets"
+  namespace        = "external-secrets-system"
   create_namespace = true
 
   version = "0.9.11"
@@ -109,7 +153,117 @@ resource "helm_release" "external_secrets" {
     aws_eks_pod_identity_association.external_secrets
   ]
 }
+
 # external-dns
+data "aws_iam_policy_document" "external_dns_assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["pods.eks.amazonaws.com"]
+    }
+
+    actions = [
+      "sts:AssumeRole",
+      "sts:TagSession"
+    ]
+  }
+}
+
+data "aws_iam_policy_document" "external_dns" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "route53:ChangeResourceRecordSets",
+      "route53:ListResourceRecordSets"
+    ]
+
+    resources = ["arn:aws:route53:::hostedzone/*"]
+  }
+
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "route53:ListHostedZones"
+    ]
+
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_role" "external_dns" {
+  name               = "${var.project_name}-external-dns"
+  assume_role_policy = data.aws_iam_policy_document.external_dns_assume_role.json
+
+  tags = {
+    Environment = var.environment
+    Project     = var.project_name
+    Terraform   = "true"
+  }
+}
+
+resource "aws_iam_policy" "external_dns" {
+  name   = "${var.project_name}-external-dns"
+  policy = data.aws_iam_policy_document.external_dns.json
+
+  tags = {
+    Environment = var.environment
+    Project     = var.project_name
+    Terraform   = "true"
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "external_dns" {
+  role       = aws_iam_role.external_dns.name
+  policy_arn = aws_iam_policy.external_dns.arn
+}
+
+resource "aws_eks_pod_identity_association" "external_dns" {
+  cluster_name    = module.eks.cluster_name
+  namespace       = "external-dns"
+  service_account = "external-dns"
+  role_arn        = aws_iam_role.external_dns.arn
+
+  tags = {
+    Environment = var.environment
+    Project     = var.project_name
+    Terraform   = "true"
+  }
+}
+
+resource "helm_release" "external_dns" {
+  name             = "external-dns"
+  repository       = "https://kubernetes-sigs.github.io/external-dns"
+  chart            = "external-dns"
+  namespace        = "external-dns"
+  create_namespace = true
+
+  version = "1.14.3"
+
+  values = [
+    yamlencode({
+      provider = "aws"
+      aws = {
+        region = var.aws_region
+      }
+      domainFilters = ["dev.daveops.sh"]
+      sources = [
+        "service",
+        "ingress"
+      ]
+      policy     = "sync"
+      txtOwnerId = var.project_name
+    })
+  ]
+
+  depends_on = [
+    module.eks,
+    aws_eks_pod_identity_association.external_dns
+  ]
+}
 
 # cert-manager 
 
