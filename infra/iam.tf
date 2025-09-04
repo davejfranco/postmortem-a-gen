cffc9e6cca24edba8a7675d54147092c58caf0ed
Dave Franco <self@daveops.sh>
2025-09-04 20:52:22 +0200

    bedrock policy

--- infra/iam.tf
+++ infra/iam.tf
@@ -31,3 +31,46 @@ resource "aws_ecr_repository_policy" "ecr" {
   repository = aws_ecr_repository.this.name
   policy     = data.aws_iam_policy_document.ecr.json
 }
+
+data "aws_iam_policy_document" "bedrock_policy" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "bedrock:InvokeModel",
+      "bedrock:InvokeModelWithResponseStream"
+    ]
+
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_policy" "bedrock_invoke" {
+  name   = "bedrock-invoke-policy"
+  policy = data.aws_iam_policy_document.bedrock_policy.json
+}
+
+module "postmortem_access" {
+  source  = "terraform-aws-modules/eks-pod-identity/aws"
+  version = "v2.0.0"
+
+  name = "${var.project_name}-access"
+
+  additional_policy_arns = {
+    additional           = aws_iam_policy.bedrock_invoke.arn
+  }
+
+  associations = {
+    postmortem-eks = {
+      cluster_name    = module.eks.cluster_name
+      namespace       = "default"
+      service_account = "${var.project_name}-sa"
+    }
+  }
+
+  tags = {
+    Environment = "demo"
+  }
+}
+
+
